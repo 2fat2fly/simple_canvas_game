fc1712db5d04aed257fe2510d25bdcbcbdf6ee2e
2fat2fly <muzgil@gmail.com>
2013-12-22 19:56:55 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -59,16 +59,16 @@ var reset = function () {
 
 // Update game objects
 var update = function (modifier) {
-        if (38 in keysDown && hero.y != 0) { // Player holding up
+        if (38 in keysDown && hero.y > 0) { // Player holding up
                 hero.y -= hero.speed * modifier;
         }
-        if (40 in keysDown && hero.y != canvas.height) { // Player holding down
+        if (40 in keysDown && hero.y < 480) { // Player holding down
                 hero.y += hero.speed * modifier;
         }
-        if (37 in keysDown && hero.x != canvas.width) { // Player holding left
+        if (37 in keysDown && hero.x > 0 ) { // Player holding left
                 hero.x -= hero.speed * modifier;
         }
-        if (39 in keysDown && hero.x != 0) { // Player holding right
+        if (39 in keysDown && hero.x < 512) { // Player holding right
                 hero.x += hero.speed * modifier;
         }
 
